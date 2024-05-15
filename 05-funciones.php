0337f91504a1e384b3da6f49a9dd227eabc56d34
Velasquez Luis <1478558@senati.pe>
2024-05-15 17:05:48 -0500

    se agrego funcio de calcular suma, resta ,etc

--- 05-funciones.php
+++ 05-funciones.php
@@ -7,7 +7,22 @@ function mostraDatos ($nombre, $apellido, $curso, $semestre) {
     $mensaje = " hola soy " .$nombre." ".$apellido." y estoy en el curso ".$curso." del ".$semestre;
     return $mensaje;
 }
+
+function calcular($operador,$numero1,$numero2) {
+
+    if($operador=="+")
+    return $numero1+$numero2;
+    if($operador=="-")
+    return $numero1-$numero2;
+    if($operador=="*")
+    return $numero1*$numero2;
+    if($operador=="/")
+    return $numero1/$numero2;
+}
+
 echo obtenersaludo("yonathan")."\n";
 
-echo mostraDatos("yonathan","flores","wep","III",);
+echo mostraDatos("yonathan","flores","wep","III",)."\n";
+
+echo "El total de la suma es: ".calcular("+",10,34);
 ?>
