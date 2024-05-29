d35b6b8b571271b5499093f027ad5f670230e5da
Velasquez Luis <1478558@senati.pe>
2024-05-29 16:57:42 -0500

    paciente 0

---
+++ 15.1-programa.php
@@ -0,0 +1,48 @@
+<?php
+
+include("15-polimorfismo.php");
+
+echo "\nAREA DE TRIANGULO\n";
+echo "INGRESE LA BASE : ";
+$b=fgets(STDIN);
+echo "INGRESE LA ALTURA : ";
+$h=fgets(STDIN);
+$triangulo = new Triangulo($b,$h);
+$area=$triangulo->calcularArea();
+echo "EL AREA DEL TRIANGULO ES : ".$area;
+
+echo "\n*****************************\n";
+
+echo "\nAREA DE RECTANGULO\n";
+echo "INGRESE LA BASE : ";
+$b=fgets(STDIN);
+echo "INGRESE LA ALTURA : ";
+$a=fgets(STDIN);
+$resctangulo = new Rectangulo($b,$h);
+$area=$resctangulo->calcularArea();
+echo "EL AREA DEL RECTANGULO ES : ".$area;
+
+
+echo "\n*****************************\n";
+echo "";
+
+echo "\nAREA DE CUADRADO\n";
+echo "INGRESE LA LADO : ";
+$b=fgets(STDIN);
+$cuadrado = new Cuadrado($b,$h);
+$area=$cuadrado->calcularArea();
+echo "EL AREA DEL CUADRADO ES : ".$area;
+
+
+echo "\n*****************************\n";
+echo "";
+
+echo "\nAREA DE CIRCULO\n";
+echo "INGRESE LA RADIO : ";
+$r=fgets(STDIN);
+$circulo = new Circulo($r);
+$area=$circulo->calcularArea();
+echo "EL AREA DEL CIRCULO ES : ".$area;
+
+echo "\n*****************************\n";
+?>
