91fcc1e9d170234c67bfe1bc8ee1a58956fa5d96
Velasquez Luis <1478558@senati.pe>
2024-05-29 17:50:54 -0500

    paciente 0

--- 21-pdo-post.php
+++ 21-pdo-post.php
@@ -1,5 +1,20 @@
 <?php
 
+try {
+    $dsn ="mysql:host=localhost;dbname=covid";
+    $user="root";
+    $pass="";
+    $db = new PDO($dsn, $user, $pass);
+    echo "Hola base de datos tengo conexion\n";
+    $pacientes = $db ->query('SELECT * FROM pacientes where 14<=edad AND edad<= 19');
+    foreach ($pacientes as $row) {
+        echo $row["nombres"]." ".$row["apellidos"].", EDAD :  ".$row["edad"]."\n"; 
+    }
+
+} catch (PDOException $e){
+    echo "Error : ".$e->getMessage(); 
+}
+
 if ($_SERVER ["REQUEST_METHOD"]=="POST") {
     $nombre =$_POST['nombre'];
     $apellido =$_POST['apellido'];
