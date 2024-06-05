fc100f5ee485933f174f4dfaff78338b848e05e4
yonathan flores <1536142@senati.pe>
2024-06-05 16:02:48 -0500

    busqueda de pacientes con ajax

--- 25.1-ajax.js
+++ 25.1-ajax.js
@@ -9,8 +9,27 @@ function buscarPacientes() {
     type :"post",
     data : datos,
     success : function(result) {
-        console.log(result);
+        //console.log(result);
+        const  pacientes = result;
+        pacientes.forEach(item => {
+            agregarFilas("#tabla",item);
+        });
     }
    })
 return;
+}
+
+function agregarFilas(id,paciente) {
+    const html =
+    "<tr>"+
+    "<td>"+paciente.Nombres+"</td>"+
+    "<td>"+paciente.Edad+"</td>"+
+    "<td>"+paciente.Talla+"</td>"+
+    "<td>"+paciente.Peso+"</td>"+
+    "<td>"+paciente.Tos+"</td>"+
+    "<td>"+paciente.Fiebre+"</td>"+
+    "<td>"+paciente.Disnea+"</td>"+
+    "<td><button type='button'>Editar</></td>"+
+    "</tr>";
+    $(Id + " tr:last").after(html);
 }
