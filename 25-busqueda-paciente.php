a3f8ffdac249d19c69b407465d3d47494783a45a
YoN4THa2-Fl0 <1536142@senati.pe>
2024-06-07 21:46:39 -0500

    Add files via upload

--- 25-busqueda-paciente.php
+++ 25-busqueda-paciente.php
@@ -4,30 +4,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Busqueda de Pacientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body><center>
-    <center><h1>Busqueda de Pacientes</h1></center>
-    <!---<form action="22.1-consulta-pdo.php" method="POST">-->
-    <input type="text" placeholder ="Escribe aqui" name="nombre" id="nombre" /><br>
-        <label id ="tos" name ="tos">Tos</label><input type="checkbox" for="tos">&nbsp;&nbsp;
-        <button type="button" onclick="buscarPacientes();">Buscar</button>
-        <table style="border: 3px solid black;">
-</center>
+<body>
+    <center>
+    <h1>Busqueda de Pacientes</h1>
+    <input type="text" placeholder="Escribe aqui" name="nombre" id="nombre" /><br>
+    <label id="tos" name="tos">Tos</label><input type="checkbox" id="tos" name="tos">
+    <button type="button" onclick="buscarPacientes();">Buscar</button>
+    <table style="border: 1px solid black;" id="tabla">
         <tr>
-            <td>Paciente</td>&nbsp;&nbsp;&nbsp;
+            <td>Paciente</td>
             <td>Edad</td>
             <td>Talla</td>
             <td>Peso</td>
             <td>Tos</td>
             <td>Fiebre</td>
             <td>Disnea</td>
-            <td>Dolor muscular</td>
             <td>Acciones</td>
-        </tr>
-</table>
-<!--</form> -->
-<script type="text/javascript"
-src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="25.1-ajax.js"></script>
-</center></body>
+        </tr>          
+    </table>
+
+    <script type="text/javascript"
+    src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="25.1-ajax.js"></script>
+ 
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Nuevo Paciente
+    </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Formulario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="nombres2" class="form-label">Nombre:</label>
+                    <input type="text" class="form-control" id="nombres2" name="nombres2"/>
+
+                    <label for="edad2" class="form-label">Edad:</label>
+                    <input type="text" class="form-control" id="edad2" name="edad2"/>
+
+                    <label for="talla2" class="form-label">Talla:</label>
+                    <input type="text" class="form-control" id="talla2" name="talla2"/>
+
+                    <label for="peso2" class="form-label">Peso:</label>
+                    <input type="text" class="form-control" id="peso2" name="peso2"/><br>
+
+                    <label class="form-label">Tos:</label>
+                    <input type="checkbox" id="tos2" name="tos2"/>&nbsp;&nbsp;
+
+                    <label class="form-label">Fiebre:</label>
+                    <input type="checkbox" id="fiebre2" name="fiebre2"/>&nbsp;&nbsp;
+                    
+                    <label class="form-label">Disnea:</label>
+                    <input type="checkbox" id="disnea2" name="disnea2"/>&nbsp;&nbsp;<Br>
+
+                    <button type="button" class="btn btn-primary" onclick="actualizar()">Guardar</button>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </center>
+</body>
 </html>
--- 25.1-ajax.js
+++ 25.1-ajax.js
@@ -1,38 +1,77 @@
 function buscarPacientes() {
-   const $nombre = $("#nombre").val();
-   //alert ("Buscando..."+nombre);
-   let datos ={
-    nombre : $nombre
-   };
-   $.ajax({
-    url :"26-ajax-pdo.php",
-    type :"post",
-    data : datos,
-    success : function(result) {
-        //console.log(result);
-        debugger;
-        //const  pacientes = result;
-        console.log(result);
-        const pacientes=$.parseJSON(result);
-        pacientes.forEach(item => {
-            agregarFilas("#tabla",item);
-        });
-    }
-   })
-return;
+    const $nombre = $("#nombre").val();
+    let datos = {
+        nombre: $nombre
+    };
+    $.ajax({
+        url: "26-ajax-pdo.php",
+        type: "post",
+        data: datos,
+        success: function(result) {
+            console.log(result);
+            const pacientes = $.parseJSON(result);
+            $("#tabla tr:not(:first)").remove(); // Clear existing rows except for the header
+            pacientes.forEach(item => {
+                agregarFilas("#tabla", item);
+            });
+        }
+    });
+    return;
 }
 
-function agregarFilas(id,paciente) {
+function agregarFilas(id, paciente) {
     const html =
-    "<tr>"+
-    "<td>"+paciente.nombres+"</td>"+
-    "<td>"+paciente.edad+"</td>"+
-    "<td>"+paciente.talla_m+"</td>"+
-    "<td>"+paciente.peso_kg+"</td>"+
-    "<td>"+paciente.sintomas_tos+"</td>"+
-    "<td>"+paciente.sintomas_fiebre+"</td>"+
-    "<td>"+paciente.sintoma_disnea+"</td>"+
-    "<td><button type='button'>Editar</></td>"+
-    "</tr>";
-    $(id+" tr:last").after(html);
+        "<tr>" +
+        "<td>" + paciente.nombres + "</td>" +
+        "<td>" + paciente.edad + "</td>" +
+        "<td>" + paciente.talla_m + "</td>" +
+        "<td>" + paciente.peso_kg + "</td>" +
+        "<td>" + (paciente.sintoma_tos ? "Sí" : "No") + "</td>" +
+        "<td>" + (paciente.sintoma_fiebre ? "Sí" : "No") + "</td>" +
+        "<td>" + (paciente.sintoma_disnea ? "Sí" : "No") + "</td>" +
+        "<td><button type='button' onclick=editar('" + paciente.nombres + "','" + paciente.edad + "','" + paciente.talla_m + "','" + paciente.peso_kg + "','" + paciente.sintoma_tos + "','" + paciente.sintoma_fiebre + "','" + paciente.sintoma_disnea + "');>Editar</button></td>" +
+        "</tr>";
+    $(id + " tr:last").after(html);
+}
+
+function editar(nombres, edad, talla_m, peso_kg, sintoma_tos, sintoma_fiebre, sintoma_disnea) {
+    $('#exampleModal').modal('show');
+    $("#nombres2").val(nombres);
+    $("#edad2").val(edad);
+    $("#talla2").val(talla_m);
+    $("#peso2").val(peso_kg);
+    $("#tos2").prop('checked', sintoma_tos === "Sí");
+    $("#fiebre2").prop('checked', sintoma_fiebre === "Sí");
+    $("#disnea2").prop('checked', sintoma_disnea === "Sí");
+}
+
+function actualizar() {
+    const nombre = $("#nombres2").val();
+    const edad = $("#edad2").val();
+    const talla = $("#talla2").val();
+    const peso = $("#peso2").val();
+    const tos = $("#tos2").is(':checked') ? "Sí" : "No";
+    const fiebre = $("#fiebre2").is(':checked') ? "Sí" : "No";
+    const disnea = $("#disnea2").is(':checked') ? "Sí" : "No";
+    
+    let datos = {
+        nombres: nombre,
+        edad: edad,
+        talla_m: talla,
+        peso_kg: peso,
+        sintoma_tos: tos,
+        sintoma_fiebre: fiebre,
+        sintoma_disnea: disnea
+    };
+
+    $.ajax({
+        url: "26-ajax-pdo.php",
+        type: "post",
+        data: datos,
+        success: function(response) {
+            console.log(response);
+            $('#exampleModal').modal('hide');
+            buscarPacientes();
+        }
+    });
 }
